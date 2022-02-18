c4b83735946713ce7ccfff0f3abd9a3ea59802db
jay-io <jay.dnb@outlook.in>
2022-02-18 20:29:57 +0530

    Add linebreaks to enhance readability

--- include/WN3_Path.h
+++ include/WN3_Path.h
@@ -1,10 +1,14 @@
 // index file path relative to project root
 #define WN3DB_IDX_POS_PATH "wndb3/index/" 
+
 // parts of speech data file path relative to project root
 #define WN3DB_DAT_POS_PATH "wndb3/data/"
+
 // word inflection list file path relative to project root
 #define WN3DB_EXC_POS_PATH "wndb3/exc/"
+
 // lexicographer'ss file path relative to project root
 #define WN3DB_LEXFILE_FILE "wndb3/lex/"
+
 // verb frame file and related resources relative to project root
 #define WN3DB_VFRAMES_FILE "wndb3/frames/"
