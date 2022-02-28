25e33bdb51cf19219c2c21008f6e17bcf16fd218
jay-io <jay.dnb@outlook.in>
2022-02-28 06:45:03 +0530

    Add test module for class jay_io::Index

---
+++ test/index_test.cc
@@ -0,0 +1,8 @@
+#include "../include/index.h"
+using namespace std;
+
+int main()
+{
+        jay_io::Index i("abkhaz n 3 6 @ #m #p %m ; - 3 0 09660722 09041725 08504963  ");
+        i.previewIndex();
+}
