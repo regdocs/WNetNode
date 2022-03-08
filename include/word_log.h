92fa80894f6e359f18e935ecb0835f5f420e4b46
jay-io <jay.dnb@outlook.in>
2022-03-08 19:11:48 +0530

    Add compatibility for null cases, data preview function

--- include/word_log.h
+++ include/word_log.h
@@ -14,8 +14,48 @@ namespace jay_io
 
 struct Word_Log
 {
+        bool containsCargo;
         std::string word;
+
+        // if contains cargo, use:
         std::vector<POS_Group> posGroups;
+
+        // if does not contain cargo (no match found), use:
+        // also write Suggestion(), Suggestion::suggestor()
+        std::vector<std::string> suggestions;
+
+        void previewWordLog()
+        {
+                using namespace std;
+
+                auto prettyPrintVectorOfStrings = [&] (std::vector<std::string> &vos) -> std::string {
+                        std::string a, t;
+                        if (vos.size() == 0)
+                                return "-";
+
+                        for (int i = 0; i < vos.size(); i++)
+                        {
+                                if (vos.size() - 1 != i) {
+                                        t = vos[i];
+                                        t.append(", ");
+                                        a.append(t);
+                                } else
+                                        a.append(vos[i]);
+                        }
+                        return a;
+                };
+
+                cout << "Word: " << word << endl << endl;
+                for (auto i: posGroups) {
+                        cout << "Part of Speech: " << i.prettyPos << endl;
+                        cout << "Inflections: " << prettyPrintVectorOfStrings(i.inflections) << endl;
+                        cout << "Pronunciation: " << prettyPrintVectorOfStrings(i.pronunciations) << endl;
+                        cout << "Definitions: " << endl;
+                        for (auto i: i.prettySenses)
+                                i.previewSenseInterface();
+                        cout << endl << endl;
+                }
+        }
 };
 
 }
