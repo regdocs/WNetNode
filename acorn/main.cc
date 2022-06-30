745384764293ec933561ded4a89de8a90d11a124
jay-io <jay.dnb@outlook.in>
2022-06-30 06:21:13 +0530

    Add ANSI colour codes to command line output

--- acorn/main.cc
+++ acorn/main.cc
@@ -2,7 +2,7 @@
 int main()
 {
         while (true) {
-                std::cout << "Enter query +:> ";
+                std::cout << "\x1b[34;1mEnter query: \x1b[0m ";
                 std::string query;
                 std::getline(std::cin, query);
                 jay_io::Acorn acorn(ACORN_MODE_EXECMD);
